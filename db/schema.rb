a272c733f375f3d58cbc9815be6a926dd2a15cc5
kouhei55555 <141524423+kouhei55555@users.noreply.github.com>
2023-10-13 00:59:00 +0000

    Finish

--- db/schema.rb
+++ db/schema.rb
@@ -43,9 +43,9 @@ ActiveRecord::Schema.define(version: 2023_10_06_192739) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
